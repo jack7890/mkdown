ad676df5a64da1b12441f35b109db6efc621a1b5
Jack Groetzinger <jack@seatgeek.com>
2014-01-17 21:38:26 -0500

    Closes #9.

--- public/js/ga.js
+++ public/js/ga.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-46773570-1', 'mkdwn.com');
+ga('create', 'UA-47244145-1', 'mkdown.com');
 ga('send', 'pageview');
